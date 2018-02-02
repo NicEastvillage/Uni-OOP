e0fd96031e7e039f4e6dfa60860ae7696eef0b0e
NicEastvillage <nicoesterby@gmail.com>
2018-02-02 12:46:12 +0100

    Added problem 5

--- src/week0/problem5/FeetMeter.java
+++ src/week0/problem5/FeetMeter.java
@@ -1,4 +1,17 @@
 package week0.problem5;
 
+import java.util.Scanner;
+
 public class FeetMeter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input feet: ");
+        Double feet = sc.nextDouble();
+        Double meter = feet * 0.3048;
+
+        System.out.println(feet + " feet is " + meter + " meter");
+    }
 }
