6c796f7e4c477a343d0efe77710ab1d88d5fa4d1
NicEastvillage <nicoesterby@gmail.com>
2018-02-02 12:45:53 +0100

    Added problem 4

---
+++ src/week0/Problem4/HelloName.java
@@ -0,0 +1,15 @@
+package week0.Problem4;
+
+import java.util.Scanner;
+
+public class HelloName {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
