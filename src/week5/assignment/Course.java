445636faf1374b2e4d29b78523861d4da9cb3d18
NicEastvillage <nicoesterby@gmail.com>
2018-03-20 20:30:53 +0100

    Scrapped everything

--- src/week5/assignment/Course.java
+++ src/week5/assignment/Course.java
@@ -1,65 +1,21 @@
 package week5.assignment;
 
-import java.util.HashSet;
-import java.util.function.Consumer;
-
 public class Course {
 
-    private Professor professor;
+    private Professor lecturer;
     private final String name;
     private final String shortName;
-    private HashSet<Student> attendingStudents;
-    private Consumer<Course> professorChangeCallback;
-    private boolean paused;
 
     /** A Course */
-    public Course(Professor professor, String name, String shortName) {
-        this.professor = professor;
+    public Course(Professor lecturer, String name, String shortName) {
+        this.lecturer = lecturer;
         this.name = name;
         this.shortName = shortName;
-        attendingStudents = new HashSet<>();
-        paused = professor == null;
-    }
-
-    /** Make a Student attend this Course. */
-    public void addStudent(Student student) {
-        attendingStudents.add(student);
-        student.attend(this);
-    }
-
-    /** Make a Student stop attending this Course. */
-    public void removeStudent(Student st) {
-        attendingStudents.remove(st);
-        st.stopAttending(this);
-    }
-
-    /** @return true if {@code student} attends this Course. */
-    public boolean doesStudentAttend(Student student) {
-        return attendingStudents.contains(student);
-    }
-
-    /** Set the Professor that lectures this Course replacing the previous lecturer. If set to null the Course will
-     * be marked as paused and can only resumed when a new non-Professor is set as lecturer.
-     * @param professorChangeCallback a callback with this Course as the parameter. Called once when this method is
-     *                                called again. Can be used to notify the previous lecturer, that he or she
-     *                                is no longer the lecturer of this Course. Can be null. */
-    public void setProfessor(Professor professor, Consumer<Course> professorChangeCallback) {
-        if (professorChangeCallback != null) {
-            this.professorChangeCallback.accept(this);
-        } else if (professor == null) {
-            // Pause the course if it has no lecturer
-            pause();
-        } else {
-            professor.lectureCourse(this);
-        }
-
-        this.professor = professor;
-        this.professorChangeCallback = professorChangeCallback;
     }
 
     /** @return the Professor that lectures this Course. */
-    public Professor getProfessor() {
-        return professor;
+    public Professor getLecturer() {
+        return lecturer;
     }
 
     /** The name of this Course. */
@@ -70,36 +26,6 @@ public class Course {
     /** The short name of this Course, e.g. CS is short for Computer Science. */
     public String getShortName() {
         return shortName;
-    }
-
-    /** @return a HashSet of Students attending this Course. */
-    public HashSet<Student> getAttendingStudents() {
-        return new HashSet<>(attendingStudents);
-    }
-
-    /** Pause this Course. Can be resumed with {@code start()}. */
-    public void pause() {
-        paused = true;
-    }
-
-    /** @return true if this Course is paused. */
-    public boolean isPaused() {
-        return paused;
-    }
-
-    /** @return true if this Course can be started/resumed or if it isn't paused. A Course must have a lecturer to start. */
-    public boolean canStart() {
-        return !paused || professor != null;
-    }
-
-    /** Start a paused Course. A Course must have a lecturer to start.
-     * @return true if this Course can be started/resumed or if it isn't paused. */
-    public boolean start() {
-        if (canStart()) {
-            paused = false;
-            return true;
-        }
-        return false;
     }
 
     /** @return the {@code <courseName> (<shortName>)}.*/
--- src/week5/assignment/Main.java
+++ src/week5/assignment/Main.java
@@ -1,15 +1,13 @@
 package week5.assignment;
 
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
         University university = new University("Winterspring University");
 
         // Professors
-        Professor prfEdinson = new Professor("Jules Edison");
+        Professor prfEdinson = new Professor("Kasper Edison");
         Professor prfHawking = new Professor("Valdemar Hawking");
-        Professor prfKennedy = new Professor("Alexandre Kennedy");
+        Professor prfKennedy = new Professor("Anders Kennedy");
 
         // Courses
         Course coCALC = new Course(prfEdinson, "Calculus", "CALC");
@@ -26,44 +24,6 @@ public class Main {
         Student stKronborg = new Student("Christine Kronborg");
         Student stRadcliff = new Student("Jacob Radcliff");
 
-        // Register
-        university.registerProfessor(prfEdinson);
-        university.registerProfessor(prfHawking);
-        university.registerProfessor(prfKennedy);
-        university.addCourse(coCALC);
-        university.addCourse(coLIAL);
-        university.addCourse(coCART);
-        university.addCourse(coSTAT);
-        university.registerStudent(stMort);
-        university.registerStudent(stKristensen);
-        university.registerStudent(stSmidth);
-        university.registerStudent(stZakariasen);
-        university.registerStudent(stFriis);
-        university.registerStudent(stKronborg);
-        university.registerStudent(stRadcliff);
 
-        // Attend courses
-        university.getCourses().forEach(co -> university.getStudents().forEach(st -> st.attend(co)));
-        stSmidth.stopAttending(coCART);
-        stFriis.stopAttending(coCALC);
-        stKronborg.stopAttending(coCALC);
-        stKronborg.stopAttending(coSTAT);
-
-        // Complete courses
-        stZakariasen.complete(coCALC);
-        stMort.complete(coSTAT);
-        stSmidth.complete(coLIAL);
-
-        // Print courses
-        for (Course course : university.getCourses()) {
-            System.out.println(course.toString() + " is lectured by " + course.getProfessor() + ".");
-            Set<Student> students = course.getAttendingStudents();
-            if (students.size() == 0) {
-                System.out.println("The course has no attending students.");
-            } else {
-                System.out.println("Attending students are:");
-                students.forEach(System.out::println);
-            }
-        }
     }
 }
