a34a569181b635e17b4edbc460b0057d3f56cfd9
NicEastvillage <nicoesterby@gmail.com>
2018-03-20 21:24:31 +0100

    Added comments to the University class in w5as

--- src/week5/assignment/University.java
+++ src/week5/assignment/University.java
@@ -10,6 +10,7 @@ public class University {
     private HashSet<Professor> professors;
     private HashSet<Student> students;
 
+    /** A University helps organizing Students, Professors and Courses. */
     public University(String name) {
         this.name = name;
         courses = new HashSet<>();
@@ -17,14 +18,17 @@ public class University {
         students = new HashSet<>();
     }
 
+    /** @return the name of the University. */
     public String getName() {
         return name;
     }
 
+    /** Add a Course at University. */
     public void addCourse(Course co) {
         courses.add(co);
     }
 
+    /** Remove a Course at the University. */
     public void removeCourse(Course co) {
         if (courses.remove(co)) {
             co.setProfessor(null, null);
@@ -32,42 +36,52 @@ public class University {
         }
     }
 
+    /** @return a HashSet of all Courses at the University. */
     public HashSet<Course> getCourses() {
         return new HashSet<>(courses);
     }
 
+    /** Register a Student at University.*/
     public void registerStudent(Student student) {
         students.add(student);
     }
 
+    /** Unregister a Student at the University. This will make them stop attending any Courses. */
     public void unregisterStudent(Student student) {
         if (students.remove(student)) {
             student.getAttendedCourses().forEach(student::stopAttending);
         }
     }
 
+    /** @return true, if the Student is registered at the University. */
     public boolean isStudentRegistered(Student student) {
         return students.contains(student);
     }
 
+    /** @return a HashSet of all Students registered at the University. */
     public HashSet<Student> getStudents() {
         return new HashSet<>(students);
     }
 
+    /** Register a Professor at University. */
     public void registerProfessor(Professor professor) {
         professors.add(professor);
     }
 
+    /** Unregister a Student at the University. Any Course lecturer by this Professor will be paused until
+     * a new lecturer for the Course is set. */
     public void unregisterProfessor(Professor professor) {
         if (professors.remove(professor)) {
             professor.getCoursesLectured().forEach(c -> c.setProfessor(null, null));
         }
     }
 
+    /** @return true, if the Professor is registered at the University. */
     public boolean isProfessorRegistered(Professor professor) {
         return professors.contains(professor);
     }
 
+    /** @return a HashSet of all Professors registered at the University. */
     public HashSet<Professor> getProfessors() {
         return new HashSet<>(professors);
     }
