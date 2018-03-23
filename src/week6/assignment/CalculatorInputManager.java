835ed23e3ae37dad7c0a6b64a7829cc00196e26e
NicEastvillage <nicoesterby@gmail.com>
2018-03-23 21:21:08 +0100

    Added the CalculatorInputManager::addCharacter method in w6a.

--- src/week6/assignment/CalculatorInputManager.java
+++ src/week6/assignment/CalculatorInputManager.java
@@ -21,10 +21,12 @@ public class CalculatorInputManager {
         }
 
         // Cheesy getters
+        public String getString() { return string; }
+        public InputPartType getType() { return type; }
+        public boolean isEmpty() { return type == InputPartType.EMPTY; }
         public int getStartIndex() { return startIndex; }
         public int getLength() { return length; }
         public int getEndIndex() { return startIndex + length; }
-        public boolean isEmpty() { return type == InputPartType.EMPTY; }
         public boolean containsComma() { return comma; }
 
         public boolean canAddChar(char c) {
@@ -48,6 +50,10 @@ public class CalculatorInputManager {
                 else if (c == '+' || c == '-' || c == '*' || c == '/') type = InputPartType.OPERATOR;
             }
         }
+
+        public void removeChar() {
+            // TODO: When removing comma, set containsComma = false
+        }
     }
 
     private String input;
@@ -55,16 +61,45 @@ public class CalculatorInputManager {
     private ArrayList<InputPart> parts;
 
     public CalculatorInputManager() {
+        clear();
+    }
+
+    public void clear() {
         input = "";
         currentPart = new InputPart(0);
         parts = new ArrayList<>();
     }
 
     public void addCharacter(char c) {
+        // Ignore request if double comma
+        if (currentPart.containsComma() && c == '.') return;
 
+        try {
+            if (currentPart.canAddChar(c)) {
+                // Add to current part
+                currentPart.addChar(c);
+            } else {
+                // Create new part
+                parts.add(currentPart);
+                currentPart = new InputPart(input.length());
+                currentPart.addChar(c);
+            }
+            input += c;
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("The character '" + c + "' is not valid in a calculator.");
+        }
     }
 
     public double calculate() {
         return 0;
+    }
+
+    private boolean canCalculate() {
+        // Input must be at least 3 parts long and last one cannot be an operator
+        return false;
+    }
+
+    public String getInputString() {
+        return input;
     }
 }
