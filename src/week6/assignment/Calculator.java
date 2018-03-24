1154078038c34f121041981b1a7f632f8fbfc5d8
NicEastvillage <nicoesterby@gmail.com>
2018-03-24 09:33:44 +0100

    Improved the looks of the calculator in w6a.

--- src/week6/assignment/Calculator.java
+++ src/week6/assignment/Calculator.java
@@ -1,14 +1,28 @@
 package week6.assignment;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Calculator extends Application {
+
+    private static final double RES_HEIGHT = 82;
+    private static final Font RES_FONT = new Font(20);
+    private static final Insets RES_PADDING = new Insets(10);
+
+    private static final double BUTTON_WIDTH = 73;
+    private static final double BUTTON_HEIGHT = 54;
+    private static final Font BUTTON_FONT = new Font(20);
+
+    private static final double WINDOW_WIDTH = BUTTON_WIDTH * 3 + 2;
+    private static final double WINDOW_HEIGHT = BUTTON_HEIGHT * 4 + RES_HEIGHT;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -18,11 +32,15 @@ public class Calculator extends Application {
     public void start(Stage stage) throws Exception {
 
         // Root component
-        HBox root = new HBox();
+        VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
 
         // Top label
-        Label statusLabel = new Label("Res:");
+        Label statusLabel = new Label("0");
+        statusLabel.setMinSize(WINDOW_WIDTH, RES_HEIGHT);
+        statusLabel.setFont(RES_FONT);
+        statusLabel.setAlignment(Pos.CENTER_RIGHT);
+        statusLabel.setPadding(RES_PADDING);
         root.getChildren().add(statusLabel);
         statusLabel.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
 
@@ -31,7 +49,7 @@ public class Calculator extends Application {
         root.getChildren().add(grid);
 
         // Scene and show
-        Scene scene = new Scene(root, 350, 210);
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setScene(scene);
         stage.setTitle("Calculator");
         stage.show();
@@ -42,6 +60,8 @@ public class Calculator extends Application {
 
         for (int i = 0; i < 10; i++) {
             Button button = new Button("" + i);
+            button.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+            button.setFont(BUTTON_FONT);
             int col, row;
             if (i == 0) {
                 col = 1;
@@ -54,8 +74,13 @@ public class Calculator extends Application {
         }
 
         Button commaButton = new Button(".");
+        commaButton.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        commaButton.setFont(BUTTON_FONT);
         grid.add(commaButton, 0, 3);
+
         Button resButton = new Button("=");
+        resButton.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        resButton.setFont(BUTTON_FONT);
         grid.add(resButton, 2, 3);
 
         return grid;
