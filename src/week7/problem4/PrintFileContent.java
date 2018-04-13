974ccbaff2e18e44393a1dc74e3806fe5afbac34
NicEastvillage <nicoesterby@gmail.com>
2018-04-13 13:00:11 +0200

    Renamed w7p4.

--- src/week7/problem4/PrintFileContent.java
+++ src/week7/problem4/PrintFileContent.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class LineInFile {
+public class PrintFileContent {
 
     public static void main(String[] args) {
         if (args.length != 1) {
