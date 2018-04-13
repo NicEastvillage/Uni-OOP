c5a0ca673848b4f2e9cb7ce025b7ce3c5b9d5e04
NicEastvillage <nicoesterby@gmail.com>
2018-04-13 12:58:34 +0200

    Added w7p4.

---
+++ src/week7/problem4/LineInFile.java
@@ -0,0 +1,35 @@
+package week7.problem4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class LineInFile {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Err: First argument should be a file path, and second argument should be a line number.");
+            return;
+        }
+
+        Path path = Paths.get(args[0]);
+
+        if (!Files.exists(path) && !Files.isRegularFile(path) && !Files.isReadable(path)) {
+            System.out.println("Err: Could not find/read file at path.");
+            return;
+        }
+
+        try (BufferedReader bf = Files.newBufferedReader(path)) {
+            String line;
+            int lineNumber = 1;
+            while ((line = bf.readLine()) != null) {
+                System.out.println(line);
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            System.out.println("Err: Unable to read file.");
+        }
+    }
+}
