f22406b5aabe8346acc219d4a18b47489671de03
NicEastvillage <nicoesterby@gmail.com>
2018-04-13 13:03:51 +0200

    Revert "Renamed w7p4."
    This reverts commit 974ccbaff2e18e44393a1dc74e3806fe5afbac34.

--- src/week7/problem4/LineInFile.java
+++ src/week7/problem4/LineInFile.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PrintFileContent {
+public class LineInFile {
 
     public static void main(String[] args) {
         if (args.length != 1) {
