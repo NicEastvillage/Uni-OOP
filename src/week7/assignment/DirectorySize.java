b62a3f67ef5c0966004d369944304204cd270e4a
NicEastvillage <nicoesterby@gmail.com>
2018-04-19 22:06:18 +0200

    Cleaned w7a

--- src/week7/assignment/DirectorySize.java
+++ src/week7/assignment/DirectorySize.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.*;
 
 public class DirectorySize {
+
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Err: First argument must be a path to a directory.");
@@ -11,6 +12,10 @@ public class DirectorySize {
         }
 
         Path rootPath = Paths.get(args[0]);
+
+        /* Considered putting following if-statements in a method called isValidDirectory(Path) which returned a
+        boolean, so main would be less cluttered and "clean code". But I also wanted to have the correct error
+        message. This could be done with Exceptions, but the try-catch-block would be just as big. */
 
         if (!Files.exists(rootPath)) {
             System.out.println("Err: Directory does not exist.");
@@ -25,17 +30,19 @@ public class DirectorySize {
         try {
             long size = getSizeOfDirectoryRecursively(rootPath);
             System.out.println("Directory: " + rootPath);
-            System.out.println("size: " + size);
+            System.out.println("size: " + size + "bytes");
 
         } catch (IOException e) {
             System.out.println("Err: Could not calculate directory size.");
         }
     }
 
+    /** Return the size of a directory in bytes. */
     static long getSizeOfDirectoryRecursively(Path path) throws IOException {
         return Files.list(path)
                 .mapToLong(p -> {
                     if (Files.isRegularFile(p)) {
+                        // Regular file
                         try {
                             return Files.size(p);
                         } catch (IOException e) {
@@ -43,12 +50,14 @@ public class DirectorySize {
                         }
                     }
                     else if (!Files.isSymbolicLink(p)) {
+                        // Directory - recursion!
                         try {
                             return getSizeOfDirectoryRecursively(p);
                         } catch (IOException e) {
                             return 0;
                         }
                     } else {
+                        // Something else - don't count
                         return 0;
                     }
                 })
